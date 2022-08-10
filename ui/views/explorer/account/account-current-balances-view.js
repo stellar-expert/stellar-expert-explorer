d505bbc580b4324025794768d99f7961bb4d9e05
orbitlens <orbit.lens@gmail.com>
2022-08-10 20:59:55 +0300

    Fix method name after dependency update

--- ui/views/explorer/account/account-current-balances-view.js
+++ ui/views/explorer/account/account-current-balances-view.js
@@ -9,7 +9,7 @@ function calculateAvailableBalance(account, balance) {
     let available = new Bignumber(balance.balance).minus(new Bignumber(balance.selling_liabilities))
     if (balance.asset_type === 'native') {
         const reserves = 2 + account.subentry_count + account.num_sponsoring - account.num_sponsored
-        available = available.minus(new Bignumber(reserves).mul(new Bignumber(0.5)))
+        available = available.minus(new Bignumber(reserves).multipliedBy(new Bignumber(0.5)))
     }
     return available.toString()
 }
