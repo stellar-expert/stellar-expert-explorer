812a9ce397d9e45ecde25f812196b356a11c6207
orbitlens <orbit.lens@gmail.com>
2022-10-27 02:05:12 +0300

    Fix muxed accounts encoding in operations history results

--- api/utils/muxed-account-encoder.js
+++ api/utils/muxed-account-encoder.js
@@ -9,7 +9,7 @@ const {StrKey} = require('stellar-sdk')
 function encodeOperationMuxedAccount(baseAddress, accountAndMuxedId) {
     if (!accountAndMuxedId) return baseAddress
     const base = StrKey.decodeEd25519PublicKey(baseAddress),
-        muxedId = accountAndMuxedId.id.slice(4, 8),
+        muxedId = accountAndMuxedId.id.slice(4, 12),
         combined = Buffer.concat([base, muxedId], 40)
     return StrKey.encodeMed25519PublicKey(combined)
 }
