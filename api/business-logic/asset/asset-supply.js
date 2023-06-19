a70f54447ae17f6ed3737ffed835693651dcd052
orbitlens <orbit.lens@gmail.com>
2023-06-19 20:21:42 +0300

    Fix broken API endpoints

--- api/business-logic/asset/asset-supply.js
+++ api/business-logic/asset/asset-supply.js
@@ -1,97 +1,19 @@
 const db = require('../../connectors/mongodb-connector')
 const {validateNetwork, validateAssetName} = require('../validators')
 const errors = require('../errors')
-const {resolveAssetId} = require('./asset-resolver')
-
-async function fetchLiquidityPoolsSupply(assetIds, network) {
-    return await db[network].collection('liquidity_pools').aggregate([
-        {
-            $match: {asset: {$in: assetIds}, shares: {$gt: 0}}
-        },
-        {
-            $project: {
-                asset: 1,
-                reserves: 1
-            }
-        },
-        {
-            $unwind: {
-                path: '$asset',
-                includeArrayIndex: 'idx'
-            }
-        },
-        {
-            $match: {asset: {$in: assetIds}}
-        },
-        {
-            $group: {
-                _id: '$asset',
-                supply: {$sum: {$arrayElemAt: ['$reserves', '$idx']}}
-            }
-        }
-    ]).toArray()
-}
-
-async function fetchClaimableBalancesSupply(assetIds, network) {
-    return await db[network].collection('claimable_balances').aggregate([
-        {
-            $match: {asset: {$in: assetIds}, deleted: {$exists: false}}
-        },
-        {
-            $group: {
-                _id: '$asset',
-                supply: {$sum: '$amount'}
-            }
-        }
-    ], {hint: {asset: 1}}).toArray()
-}
-
-async function fetchTrustlinesSupply(assetIds, network) {
-    return await db[network].collection('trustlines').aggregate([
-        {
-            $match: {asset: {$in: assetIds}}
-        },
-        {
-            $group: {
-                _id: '$asset',
-                supply: {$sum: '$balance'}
-            }
-        }
-    ], {hint: {asset: 1}}).toArray()
-}
-
-/**
- *
- * @param {Number[]} assetIds
- * @param {String} network
- * @return {Promise<Object.<String,Number>>}
- */
-async function fetchAssetsSupply(assetIds, network) {
-    const promises = [fetchTrustlinesSupply, fetchLiquidityPoolsSupply]
-        .map(f => f.call(null, assetIds, network))
-    const supplyResults = await Promise.all(promises)
-    const supplyMap = {}
-    for (const res of supplyResults) {
-        for (const {_id, supply} of res) {
-            const prevSupply = supplyMap[_id] || 0
-            supplyMap[_id] = prevSupply + (typeof supply === 'number' ? supply : supply.toNumber())
-        }
-    }
-    return supplyMap
-}
+const AssetDescriptor = require('./asset-descriptor')
 
 async function queryAssetSupply(network, asset) {
     validateNetwork(network)
     validateAssetName(asset)
 
-    const assetId = await resolveAssetId(network, asset)
-    if (assetId === null)
+    const a = await db[network].collection('assets')
+        .findOne({name: new AssetDescriptor(asset).toFQAN()}, {projection: {supply: 1}})
+
+    if (!a)
         throw errors.notFound('Asset supply was not found on the ledger. Check if you specified the asset correctly.')
 
-    const res = await fetchAssetsSupply([assetId], network)
-    const supply = res[assetId]
-
-    return (supply / 10000000).toFixed(7)
+    return (a.supply / 10000000).toFixed(7)
 }
 
-module.exports = {queryAssetSupply, fetchAssetsSupply}
+module.exports = {queryAssetSupply}
--- api/business-logic/operation/payment-locator.js
+++ api/business-logic/operation/payment-locator.js
@@ -1,51 +1,7 @@
-const {Long} = require('bson')
-//const OperationsQuery = require('./operations-query')
-const {parseGenericId} = require('../../utils/generic-id-utils')
-const {validateAssetName, validateAccountAddress} = require('../validators')
-const {resolveFederationAccountAddress} = require('../federation/federation-address-resolver')
+const errors = require('../errors')
 
-async function searchPayments(network, basePath, query) {
-    const opQuery = new OperationsQuery(network, basePath, query)
-    opQuery.addTypesFilter([0, 1, 2, 8, 13])
-
-    const {memo, amount, asset, account, from, to} = query
-
-    if (asset) {
-        validateAssetName(asset)
-        await opQuery.addAssetFilter(asset)
-    }
-
-    let accounts = [account, from, to].filter(a => !!a)
-    if (accounts.length) {
-        accounts.forEach(a => validateAccountAddress(a))
-        accounts = await Promise.all(accounts.map(a => resolveFederationAccountAddress(a)))
-        await opQuery.addAccountFilter(accounts)
-    }
-
-    if (memo) {
-        await opQuery.addMemoFilter(memo)
-    }
-
-    if (amount) {
-        await opQuery.addAmountFilter(amount)
-    }
-
-    const res = await opQuery.toArray()
-
-    for (let record of res._embedded.records) {
-        const {ledger, tx} = parseGenericId(Long.fromString(record.id))
-        record.ledger = ledger
-        record.tx_id = tx
-        record.optype = record.type
-        record.ts = new Date(record.ts * 1000)
-        record.from = record.accounts[0]
-        record.to = record.accounts[record.accounts.length - 1]
-        record.asset = record.assets[record.assets.length - 1]
-        record.source_asset = record.assets[0]
-        record.source_amount = record.source_amount || record.amount
-    }
-
-    return res
+function searchPayments(network, basePath, query) {
+    return Promise.reject(errors.notFound())
 }
 
 module.exports = {searchPayments}
