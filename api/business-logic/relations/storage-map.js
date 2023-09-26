956237f1492fd27a3a27a31775d3050c631ce4e2
orbitlens <orbit.lens@gmail.com>
2023-09-26 12:50:14 +0300

    Boilerplate file reading

--- api/business-logic/relations/storage-map.js
+++ api/business-logic/relations/storage-map.js
@@ -4,7 +4,7 @@ const {StrKey} = require('stellar-sdk')
 function saveRelationsMap(address, relations) {
     if (!StrKey.isValidEd25519PublicKey(address))
         return {}
-    fs.writeFileSync(address + '.json', JSON.stringify(relations))
+    fs.writeFileSync(__dirname + '/' + address + '.json', JSON.stringify(relations))
     return {ok: 1}
 }
 
@@ -12,7 +12,7 @@ function loadRelationsMap(address) {
     try {
         if (!StrKey.isValidEd25519PublicKey(address))
             return {}
-        const raw = fs.readFileSync(address + '.json', 'utf8')
+        const raw = fs.readFileSync(__dirname + '/' + address + '.json', 'utf8')
         return JSON.parse(raw)
     } catch (e) {
         console.error(e)
