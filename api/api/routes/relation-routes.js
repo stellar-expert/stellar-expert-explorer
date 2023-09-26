fbaf4949ba75857ac7bc9ecd9e18256a6e6ca0ed
orbitlens <orbit.lens@gmail.com>
2023-09-26 12:17:20 +0300

    Change route prefixes for mappings

--- api/api/routes/relation-routes.js
+++ api/api/routes/relation-routes.js
@@ -14,11 +14,11 @@ module.exports = function (app) {
 
     registerRoute(app,
         'relations/storage-map/:address',
-        {cache: 'relations', cors: 'open'},
+        {cache: 'relations', cors: 'open', prefix:'/'},
         ({params}) => loadRelationsMap(params.address))
     registerRoute(app,
         'relations/storage-map/:address',
-        {cache: 'relations', cors: 'open', method:'post'},
+        {cache: 'relations', cors: 'open', method:'post', prefix:'/'},
         ({params, body}) => saveRelationsMap(params.address, body))
 
 
--- api/business-logic/relations/storage-map.js
+++ api/business-logic/relations/storage-map.js
@@ -3,13 +3,14 @@ const {StrKey} = require('stellar-sdk')
 
 function saveRelationsMap(address, relations) {
     if (!StrKey.isValidEd25519PublicKey(address))
-        return null
+        return {}
     fs.writeFileSync(address + '.json', JSON.stringify(relations))
+    return {ok: 1}
 }
 
 function loadRelationsMap(address) {
     if (!StrKey.isValidEd25519PublicKey(address))
-        return null
+        return {}
     const raw = fs.readFileSync(address + '.json', 'utf8')
     return JSON.parse(raw)
 }
