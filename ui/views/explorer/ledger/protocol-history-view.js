a9f9fb6f41db1bbf2e89c679ae7b32ba5d02700d
orbitlens <orbit.lens@gmail.com>
2024-01-21 16:03:02 -0100

    Update Stellar protocol upgrades history UI

--- ui/views/explorer/ledger/protocol-history-view.js
+++ ui/views/explorer/ledger/protocol-history-view.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Amount, UtcTimestamp, InfoTooltip as Info, useExplorerApi} from '@stellar-expert/ui-framework'
+import {Amount, UtcTimestamp, InfoTooltip as Info, useExplorerApi, CodeBlock, CopyToClipboard} from '@stellar-expert/ui-framework'
 import config from '../../../app-settings'
 import {setPageMetadata} from '../../../util/meta-tags-generator'
 
@@ -9,7 +9,8 @@ export default function ProtocolHistoryView() {
         title: `Protocol versions of Stellar ${config.activeNetwork} network`,
         description: `History of the Stellar ${config.activeNetwork} network protocol upgrades.`
     })
-    if (!loaded) return <div className="loader"/>
+    if (!loaded)
+        return <div className="loader"/>
     return <div className="container narrow">
         <h2>Protocol Upgrades History<Info
             link="https://www.stellar.org/developers/stellar-core/software/security-protocol-release-notes.html#list-of-releases">
@@ -17,28 +18,42 @@ export default function ProtocolHistoryView() {
             This version number is incremented every time the protocol changes over time.
         </Info></h2>
         <div className="segment blank">
-            <table className="table exportable" data-export-prefix="protocol-history">
-                <thead>
-                    <tr>
-                        <th>Upgrade Sequence</th>
-                        <th className="text-right">Protocol Version</th>
-                        <th className="text-right nowrap">Max TxSet Size</th>
-                        <th className="text-right nowrap">Base Fee</th>
-                        <th className="text-right nowrap">Base Reserve</th>
-                        <th className="text-right">Upgrade Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map(({version, sequence, ts, maxTxSetSize, baseFee, baseReserve}) => <tr key={sequence}>
-                        <td>{sequence}</td>
-                        <td className="text-right">{version}</td>
-                        <td className="text-right">{maxTxSetSize}</td>
-                        <td className="text-right"><Amount amount={baseFee} asset="XLM" adjust issuer={false}/></td>
-                        <td className="text-right"><Amount amount={baseReserve} asset="XLM" adjust issuer={false}/></td>
-                        <td className="text-right"><UtcTimestamp date={ts} className="nowrap"/></td>
-                    </tr>)}
-                </tbody>
-            </table>
+            {data.map(entry => <ProtocolHistoryEntry entry={entry} key={entry.sequence}/>)}
         </div>
+    </div>
+}
+
+function ProtocolHistoryEntry({entry}) {
+    const sorobanConfig = entry.config_changes && JSON.stringify(entry.config_changes, null, '  ')
+    return <div className="space">
+        <h3>
+            <div className="row">
+                <div className="column column-66">
+                    Ledger {entry.sequence}
+                </div>
+                <div className="column column-33 desktop-right text-small">
+                    {entry.ts > 0 && <UtcTimestamp date={entry.ts} className="nowrap"/>}
+                </div>
+            </div>
+        </h3>
+        <dl>
+            <dt>Protocol version:</dt>
+            <dd>{entry.version}</dd>
+            <dt>Maximum transaction set size:</dt>
+            <dd>{entry.max_tx_set_size}</dd>
+            <dt>Base fee amount:</dt>
+            <dd><Amount amount={entry.base_fee} asset="XLM" adjust issuer={false}/></dd>
+            <dt>Base reserve amount:</dt>
+            <dd><Amount amount={entry.base_reserve} asset="XLM" adjust issuer={false}/></dd>
+        </dl>
+        {!!sorobanConfig && <div>
+            <h4>
+                Soroban runtime config changes
+                <CopyToClipboard text={sorobanConfig} title="Copy configuration changes to the clipboard" className="text-small"/>
+            </h4>
+            <CodeBlock lang="json" style={{maxHeight: '30em'}}>{sorobanConfig}</CodeBlock>
+        </div>}
+        <div className="space"/>
+        <hr className="flare"/>
     </div>
 }
