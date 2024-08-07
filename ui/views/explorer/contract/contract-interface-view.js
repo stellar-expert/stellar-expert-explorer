41f7127a29f0073a2c66d92562f1e62e1adfdc13
orbitlens <orbit.lens@gmail.com>
2024-08-07 14:27:54 +0000

    Improve contract visualization UX

--- ui/views/explorer/contract/contract-interface-view.js
+++ ui/views/explorer/contract/contract-interface-view.js
@@ -1,6 +1,5 @@
-import {CodeBlock} from '@stellar-expert/ui-framework'
+import {CodeBlock, generateContractSourceLink, useContractSource} from '@stellar-expert/ui-framework'
 import {parseContractMetadata} from '@stellar-expert/contract-wasm-interface-parser'
-import {generateContractSourceLink, useContractSource} from '../../../business-logic/api/contract-api'
 
 export function ContractInterfaceView({hash}) {
     const source = useContractSource(hash)
@@ -20,12 +19,15 @@ export function ContractInterfaceView({hash}) {
     </div>
 }
 
+const indent = '    '
+
 function parseRustInterface(meta) {
     let res = `// RUST version: ${meta.rustVersion}\n// SDK version: ${meta.sdkVersion}\n\n`
     if (meta.functions) {
         res += '// FUNCTIONS\n\n'
-        for (const fn of meta.functions) {
-            res += `fn ${fn.name}(${fn.inputs.map(input => input.name + ': ' + input.type).join(', ')})`
+        for (const [name, fn] of Object.entries(meta.functions)) {
+            res += Object.entries(fn.inputs).map(([name, input]) => insertDocs(input, 0, name + ': ')).join('\n')
+            res += insertDocs(fn) + `fn ${name}(${Object.entries(fn.inputs).map(([name, props]) => name + ': ' + props.type).join(', ')})`
             if (fn.outputs?.length) {
                 res += ' -> ' + (fn.outputs.length > 1 ? `(${fn.outputs.join(', ')})` : fn.outputs[0])
             }
@@ -35,41 +37,49 @@ function parseRustInterface(meta) {
 
     if (meta.enums) {
         res += '// ENUMS\n\n'
-        for (const e of meta.enums) {
-            res += `#[contracttype]
-enum ${e.name} {
-    ${Object.entries(e.cases).map(([name, value]) => name + ' = ' + value.toString()).join(',\n    ')}
+        for (const [name, e] of Object.entries(meta.enums)) {
+            res += '#[contracttype]\n' + insertDocs(e) + `enum ${name} {
+    ${Object.entries(e.cases).map(([name, props]) => insertDocs(props, 1) + indent + name + ' = ' + props.value.toString()).join(',\n' + indent)}
 }\n\n`
         }
     }
 
     if (meta.structs) {
         res += '// STRUCTS\n\n'
-        for (const s of meta.structs) {
-            res += `#[contracttype]
-struct ${s.name} {
-    ${s.fields.map(field => field.name + ': ' + field.type).join(',\n    ')}
+        for (const [name, s] of Object.entries(meta.structs)) {
+            res += '#[contracttype]\n' + insertDocs(s) + `struct ${name} {
+    ${Object.entries(s.fields).map(([name, field]) => insertDocs(field, 1) + name + ': ' + field.type).join(',\n' + indent)}
 }\n\n`
         }
     }
 
     if (meta.unions) {
         res += '// UNIONS\n\n'
-        for (const u of meta.unions) {
-            res += `#[contracttype]
-enum ${u.name} {
-    ${Object.entries(u.cases).map(([name, value]) => `${name}(${value === 'void' ? '' : value.join(', ')})`).join(',\n    ')}
+        for (const [name, u] of Object.entries(meta.unions)) {
+            res += '#[contracttype]\n' + insertDocs(u) + `enum ${name} {
+    ${Object.entries(u.cases).map(([name, value]) => insertDocs(value, 1) + `${name}(${value === 'void' ? '' : value.join(', ')})`).join(',\n' + indent)}
 }\n\n`
         }
     }
 
     if (meta.errors) {
         res += '// ERRORS\n\n'
-        for (const e of meta.errors) {
-            res += `#[contracterror]
-enum ${e.name} {
-    ${e.cases.map(field => field.name + ' = ' + field.value).join(',\n    ')}
+        for (const [name, e] of Object.entries(meta.errors)) {
+            res += '#[contracttype]\n' + insertDocs(e) + `enum ${name} {
+    ${Object.entries(e.cases).map(([name, props]) => insertDocs(props, 1) + name + ' = ' + props.value).join(',\n' + indent)}
 }\n\n`
+        }
+    }
+    return res
+}
+
+function insertDocs(props, indentLevel = 0, prefix = '') {
+    if (!props.doc)
+        return ''
+    let res = '/// ' + prefix + props.doc + '\n'
+    if (indentLevel > 0) {
+        for (let i = 0; i < indentLevel; i++) {
+            res = indent + res
         }
     }
     return res
