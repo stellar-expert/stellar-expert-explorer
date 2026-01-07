7c7748526346ce02bceb35cce7d2366d987c865a
orbitlens <orbit.lens@gmail.com>
2026-01-07 14:51:01 -0100

    Temporary add price and volume to asset stats history

--- api/business-logic/asset/asset-ohlcvt.js
+++ api/business-logic/asset/asset-ohlcvt.js
@@ -1,6 +1,13 @@
 const {aggregateOhlcvt, parseBoundaries} = require('../dex/ohlcvt-aggregator')
 const {validateNetwork, validateAssetName} = require('../validators')
 
+/**
+ * Aggregate price OHLCV data for the specified asset
+ * @param {string} network
+ * @param {asset} asset
+ * @param {{}} query
+ * @return {Promise<Promise<[][]>>}
+ */
 async function aggregateAssetPriceCandlesData(network, asset, query) {
     validateNetwork(network)
     asset = validateAssetName(asset)
